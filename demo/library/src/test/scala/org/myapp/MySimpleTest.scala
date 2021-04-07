ba48f2e6cd263289b43dec42e66fa3e39be7e475
Анастасия <a9n7a3815@gmail.com>
2021-04-08 01:37:57 +0300

    changed FunSuite to FunSuite with Matchers

--- demo/library/src/test/scala/org/myapp/MySimpleTest.scala
+++ demo/library/src/test/scala/org/myapp/MySimpleTest.scala
@@ -1,9 +1,12 @@
 package org.myapp
-import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest._
+import matchers.should._
 import org.myapp.MyLibraryProvider.MY_CONST
-class MySimpleTest extends AnyFunSuite {
-  test ("foo is OKAY") {
+
+class MySimpleTest extends funsuite.AnyFunSuite with Matchers {
+  test ("foo is OK") {
     assert(MyLibraryProvider.foo(23) == 23 * MY_CONST)
   }
 
 }
+
