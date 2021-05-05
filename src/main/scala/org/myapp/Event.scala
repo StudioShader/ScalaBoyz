4e121d0770a205fa2337ae2efd4afd0fdf0dfa5e
StudioShader <studioshader2018@gmail.com>
2021-05-05 13:50:36 +0300

    final amendments on Pr2

--- src/main/scala/org/myapp/Event.scala
+++ src/main/scala/org/myapp/Event.scala
@@ -2,10 +2,6 @@ package org.myapp
 
 import java.time.LocalDate
 
-
-//import java.util.Calendar
-//import scalatime._
-
 case class Event(var date: LocalDate,
                  var name: String,
                  var notificationTime: LocalDate = LocalDate.now(),
--- src/test/scala/org/myapp/MySimpleTest.scala
+++ src/test/scala/org/myapp/MySimpleTest.scala
@@ -1,9 +1,10 @@
 package org.myapp
+
 import org.scalatest._
 import matchers.should._
 
 class MySimpleTest extends funsuite.AnyFunSuite with Matchers {
-  test ("it works") {
+  test("it works") {
     println("It works")
   }
 
